06474906c0ba7a4c7eff8c9a0c0581fca9459aa8
Russ Gunther <rgunther30@gmail.com>
2025-03-04 22:09:05 -0500

    Handle multiple pings from client

--- lib/server.ex
+++ lib/server.ex
@@ -1,4 +1,6 @@
 defmodule Server do
+  require Logger
+
   @moduledoc """
   Your implementation of a Redis server
   """
@@ -9,20 +11,35 @@ defmodule Server do
     Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
   end
 
-  @doc """
-  Listen for incoming connections
-  """
   def listen() do
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    IO.puts("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-
-    # Since the tester restarts your program quite often, setting SO_REUSEADDR
-    # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    {:ok, client} = :gen_tcp.accept(socket)
+    accept_loop(socket)
+  end
+
+  defp accept_loop(listening_socket) do
+    {:ok, client} = :gen_tcp.accept(listening_socket)
+
+    spawn(fn -> serve(client) end)
+    accept_loop(listening_socket)
+  end
+
+  defp serve(client) do
+    case :gen_tcp.recv(client, 0) do
+      {:ok, data} ->
+        Logger.info("Received: #{data}")
+        write_line(data, client)
+        serve(client)
+
+      {:error, :closed} ->
+        Logger.info("Client closed connection")
+
+      {:error, reason} ->
+        Logger.error("Error: #{reason}")
+    end
+  end
+
+  defp write_line(_line, client) do
+    # Hardcode for now per instuctions
     :gen_tcp.send(client, "+PONG\r\n")
-    :gen_tcp.close(client)
   end
 end
